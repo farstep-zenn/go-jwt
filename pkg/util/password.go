f1f229f47977065ee811e168ae764be3c7ce68ad
NaokiYazawa <yazawa.naoki.01@gmail.com>
2023-02-11 23:13:44 +0900

    add functions to hash and verify password

---
+++ pkg/util/password.go
@@ -0,0 +1,18 @@
+package util
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(hashedPassword), nil
+}
+
+func CheckPassword(hashedPassword string, inputPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(inputPassword))
+}
