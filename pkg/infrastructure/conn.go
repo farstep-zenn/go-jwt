46645b2e3e7ea5bc5ff705a7ee82e5e57a8d5e18
NaokiYazawa <yazawa.naoki.01@gmail.com>
2023-02-19 20:20:50 +0900

    load .env file

--- pkg/infrastructure/conn.go
+++ pkg/infrastructure/conn.go
@@ -6,19 +6,25 @@ import (
 	"log"
 	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 var Conn *sql.DB
 
 func init() {
+	var err error
+
+	err = godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("failed to load .env file: ", err)
+	}
+
 	user := os.Getenv("DBUser")
 	password := os.Getenv("DBPassword")
 	host := os.Getenv("DBHost")
 	port := os.Getenv("DBPort")
 	database := os.Getenv("DBName")
-
-	var err error
 
 	Conn, err = sql.Open("postgres",
 		fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable", user, password, host, port, database))
